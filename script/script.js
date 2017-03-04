33413733226f7cdc17c4fd92ce9d896616618666
egunn <erica.gunn@gmail.com>
2017-03-04 00:33:32 -0500

    add resize

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+//setup resize event listener on window (resizeView defined in view js file, since it contains d3 updates)
+window.addEventListener('resize', resizeView, false);
+
 var margin = {t:50,r:50,b:50,l:50},
     width = document.getElementById('map').clientWidth - margin.l - margin.r,
     height = document.getElementById('map').clientHeight - margin.t - margin.b;
@@ -5,13 +8,17 @@ var margin = {t:50,r:50,b:50,l:50},
 var map = d3.select('.canvas')
     .append('svg')
     .attr('width',width+margin.l+margin.r)
-    .attr('height',450+margin.t+margin.b)
+    .attr('height',height+margin.t+margin.b)
     .append('g').attr('class','map')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
 var lngLatBoston = [-71.0589,42.3601]
 
 var path;
+
+var albersProjection = d3.geo.albersUsa()
+    .translate([width/2,height/2])
+    .scale(900);
 
 //import GeoJSON data
 queue()
@@ -21,9 +28,7 @@ queue()
 
         console.log(points);
 
-        var albersProjection = d3.geo.albersUsa()
-            .translate([width/2,height/2])
-            .scale(900);
+
 
         path = d3.geo.path()
             .projection(albersProjection);
@@ -35,7 +40,7 @@ queue()
             .append('path')
             .attr('class','state')
             .attr('d',path)
-            .attr('transform','translate(20,250)')
+            //.attr('transform','translate(20,250)')
             .attr('stroke','gray')
             .attr('fill','none')
             .attr('stroke-width',1);
@@ -46,25 +51,25 @@ queue()
             .append('circle')
             .attr('cx',function(d){
                 d.x = albersProjection([d.long,+d.lat])[0];
-                return albersProjection([d.long,+d.lat])[0] +5;
+                return albersProjection([d.long,+d.lat])[0];
             })
             .attr('cy',function(d){
                 d.y = albersProjection([d.long,+d.lat])[1];
                 return albersProjection([d.long,+d.lat])[1];
             })
-            .attr('transform','translate(20,250)')
-            .attr('r',2)
+            //.attr('transform','translate(20,250)')
+            .attr('r',3)
             .attr('fill-opacity',.5)
             .attr('fill','purple')
             .on('mouseover',function(d){
                 d3.select(this).transition().attr('fill-opacity',1).attr('r',5);
 
                 map.append('text')
-                    .attr('x',d.x)
+                    .attr('x',d.x+5)
                     .attr('y',d.y)
                     .attr('font-size',12)
                     .attr('fill','gray')
-                    .attr('transform','translate(20,250)')
+                    //.attr('transform','translate(20,250)')
                     .attr('class','text-label')
                     .text(d.name);
             })
@@ -97,3 +102,29 @@ queue()
 
     });
 
+//runs on window resize, calls scale update and draw functions
+function resizeView() {
+
+    width = document.getElementById('map').clientWidth;
+    height = document.getElementById('map').clientHeight;
+
+    console.log(width, height);
+
+    albersProjection
+        .translate([width/2,height/2])
+        .scale(900);
+
+    map.selectAll('.state').attr('d',path);
+
+    map.selectAll('circle')
+        .attr('cx',function(d){
+            d.x = albersProjection([d.long,+d.lat])[0];
+            return albersProjection([d.long,+d.lat])[0];
+        })
+        .attr('cy',function(d){
+            d.y = albersProjection([d.long,+d.lat])[1];
+            return albersProjection([d.long,+d.lat])[1];
+        });
+
+
+}
