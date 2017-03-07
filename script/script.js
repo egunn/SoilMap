7610eb30fdfc33a1c27b23e06b36b5ec1df1249b
egunn <erica.gunn@gmail.com>
2017-03-06 19:43:34 -0500

    update map resize

--- script/script.js
+++ script/script.js
@@ -1,28 +1,66 @@
 //setup resize event listener on window (resizeView defined in view js file, since it contains d3 updates)
-window.addEventListener('resize', resizeView, false);
+//window.addEventListener('resize', resizeView, false);
 
 var margin = {t:50,r:50,b:50,l:50},
-    width = document.getElementById('map').clientWidth - margin.l - margin.r,
-    height = document.getElementById('map').clientHeight - margin.t - margin.b;
+    width = document.getElementById('map').clientWidth,
+    height = document.getElementById('map').clientHeight;
+
+
+console.log(width, height);
+
+var active = d3.select(null);
+var zoomedTracker = false;
 
 var map = d3.select('.canvas')
     .append('svg')
-    .attr('width',width+margin.l+margin.r)
-    .attr('height',height+margin.t+margin.b)
+    //responsive SVG needs these 2 attributes and no width and height attr
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 "+ width + " " + height)
+    //class to make it responsive
+    .classed("svg-content-responsive", true)
     .append('g').attr('class','map')
-    .attr('transform','translate('+margin.l+','+margin.t+')');
+    //.attr('transform','translate('+margin.l+','+margin.t+')')
+    .on("click", stopped, true);
+
+
+d3.select("div#vis-div")
+    .classed("svg-container", true); //container class to make it responsive
+
+
+map.append("rect")
+    .attr("class", "rect-background")
+    .attr('width',width)
+    .attr('height',height)
+    //.attr('transform','translate('+margin.l+','+margin.t+')')
+    .on("click", reset);
 
 var lngLatBoston = [-71.0589,42.3601]
 
 var path;
 
-var albersProjection = d3.geo.albersUsa()
-    .translate([width/2,height/2])
-    .scale(900);
+var zoom = d3.zoom()
+// no longer in d3 v4 - zoom initialises with zoomIdentity, so it's already at origin
+// .translate([0, 0])
+// .scale(1)
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
+
+map
+    .call(zoom);
+
+var g = map.append("g");
+
+var albersProjection = d3.geoAlbersUsa() // updated for d3 v4
+    .scale(900)
+    .translate([width / 2, (height / 2-75)]);
+
+
+
 
 //import GeoJSON data
 queue()
-    .defer(d3.json, "data/gz_2010_us_040_00_5m.json")
+    .defer(d3.json, "./data/us.json")
     .defer(d3.csv, "data/soilPoints.csv")
     .await(function(err, states, points){
 
@@ -30,25 +68,45 @@ queue()
 
 
 
-        path = d3.geo.path()
+        path = d3.geoPath() // updated for d3 v4
             .projection(albersProjection);
 
+
+        g.selectAll("path")
+            .data(topojson.feature(states, states.objects.states).features)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("class", "feature")
+            .on("click", clicked);
+
+        g.append("path")
+            .datum(topojson.mesh(states, states.objects.states, function(a, b) { return a !== b; }))
+            .attr("class", "mesh")
+            .attr("d", path);
+
+
+/*
         //Draw states as separate <path> elements
         map.selectAll('.state')
             .data(states.features)
             .enter()
             .append('path')
-            .attr('class','state')
+            .attr("class", "feature")
             .attr('d',path)
             //.attr('transform','translate(20,250)')
             .attr('stroke','gray')
             .attr('fill','none')
-            .attr('stroke-width',1);
+            .attr('stroke-width',1)
+            .on("click", clicked);
 
-        map.selectAll('.points')
+*/
+
+
+        g.selectAll('.points')
             .data(points)
             .enter()
             .append('circle')
+            .attr('class','points')
             .attr('cx',function(d){
                 d.x = albersProjection([d.long,+d.lat])[0];
                 return albersProjection([d.long,+d.lat])[0];
@@ -58,20 +116,38 @@ queue()
                 return albersProjection([d.long,+d.lat])[1];
             })
             //.attr('transform','translate(20,250)')
-            .attr('r',3)
+            .attr('r',2)
             .attr('fill-opacity',.5)
             .attr('fill','purple')
             .on('mouseover',function(d){
-                d3.select(this).transition().attr('fill-opacity',1).attr('r',5);
 
-                map.append('text')
-                    .attr('x',d.x+5)
-                    .attr('y',d.y)
-                    .attr('font-size',12)
-                    .attr('fill','gray')
-                    //.attr('transform','translate(20,250)')
-                    .attr('class','text-label')
-                    .text(d.name);
+                if (zoomedTracker == false){
+
+                    d3.select(this).transition().attr('fill-opacity',1).attr('r',5);
+
+                    g.append('text')
+                        .attr('x',d.x+5)
+                        .attr('y',d.y)
+                        .attr('font-size',12)
+                        .attr('fill','gray')
+                        //.attr('transform','translate(20,250)')
+                        .attr('class','text-label')
+                        .text(d.name);
+                }
+                else if (zoomedTracker == true){
+
+                    d3.select(this).transition().attr('fill-opacity',1).attr('r',2);
+
+                    g.append('text')
+                        .attr('x',d.x+3)
+                        .attr('y',d.y)
+                        .attr('font-size',2)
+                        .attr('fill','gray')
+                        //.attr('transform','translate(20,250)')
+                        .attr('class','text-label')
+                        .text(d.name);
+                }
+
             })
             .on('mouseout',function(d){
                 d3.select(this).transition().attr('fill-opacity',.5).attr('r',2);
@@ -102,8 +178,56 @@ queue()
 
     });
 
+function clicked(d) {
+    if (active.node() === this) return reset();
+    active.classed("active", false);
+    active = d3.select(this).classed("active", true);
+
+    var bounds = path.bounds(d),
+        dx = bounds[1][0] - bounds[0][0],
+        dy = bounds[1][1] - bounds[0][1],
+        x = (bounds[0][0] + bounds[1][0]) / 2,
+        y = (bounds[0][1] + bounds[1][1]) / 2,
+        scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+        translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+    map.transition()
+        .duration(750)
+        // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+        .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+}
+
+function reset() {
+
+    zoomedTracker = false;
+    console.log('reset');
+    active.classed("active", false);
+    active = d3.select(null);
+
+    map.transition()
+        .duration(750)
+        // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+        .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
+}
+
+function zoomed() {
+    zoomedTracker = true;
+    g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+    // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+    g.attr("transform", d3.event.transform); // updated for d3 v4
+}
+
+// If the drag behavior prevents the default click,
+// also stop propagation so we don’t click-to-zoom.
+function stopped() {
+    if (d3.event.defaultPrevented) d3.event.stopPropagation();
+}
+
+/*
 //runs on window resize, calls scale update and draw functions
 function resizeView() {
+
+    console.log('resize')
 
     width = document.getElementById('map').clientWidth;
     height = document.getElementById('map').clientHeight;
@@ -128,3 +252,4 @@ function resizeView() {
 
 
 }
+    */
